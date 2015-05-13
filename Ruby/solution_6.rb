43dce3781f9bfaa980bc66c52ae4668948b123d7
burythehammer <matt@burythehammer.com>
2015-05-13 16:03:50 +0100

    Added rubocop todo

---
+++ Ruby/solution_6.rb
@@ -0,0 +1,30 @@
+# solution_6.rb
+
+# https://projecteuler.net/problem=6
+
+# The sum of the squares of the first ten natural numbers is
+# 12 + 22 + ... + 102 = 385
+
+# The square of the sum of the first ten natural numbers is
+# (1 + 2 + ... + 10)2 = 552 = 3025
+
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+# Answer: 25164150
+
+def solution6
+  sum_of_squares = 0
+  sum = 0
+
+  (1..100).each do |i|
+    sum_of_squares += i * i
+    sum += i
+  end
+
+  abs(sum_of_squares - sum * sum)
+end
+
+require_relative 'print_solution'
+print_solution(6, 25_164_150)
